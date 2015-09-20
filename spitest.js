e3f8650a4b44fb3ebde4b5ef8b172d5814ba5c82
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-20 15:24:36 -0400

    switched to BigEndian 16bit

--- spitest.js
+++ spitest.js
@@ -9,7 +9,7 @@ var spi = new SPI.Spi('/dev/spidev0.1', {
   });
 
 var txbuf = new Buffer([ 0x23, 0x48, 0xAF, 0x19, 0x19, 0x19 ]);
-var rxbuf = new Buffer([ 0x00, 0x00, 0x00 , 0x00]);
+var rxbuf = new Buffer([ 0x00, 0x00, 0x00]);
 
 setInterval(function(){
   spi.read(rxbuf, function(device, buf) {
@@ -17,6 +17,6 @@ setInterval(function(){
     // for (var i=0; i < buf.length; i++)
     //   s = s + buf[i] + " ";
     // console.log(s);
-    console.log(buf.readUInt32LE(0));
+    console.log(buf.readUInt16BE(0));
   });
 },100);
