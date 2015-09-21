20c63932d8380e2c1eb5f63b8d92d8da6009af09
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-21 11:55:11 -0400

    Added am2302 test

---
+++ am2302test.js
@@ -0,0 +1,44 @@
+var sensorLib = require('node-dht-sensor');
+var gpio = require('rpi-gpio');
+var AM_PIN = 17;
+var AM_RESET_PIN = 22;
+
+gpio.setup(AM_PIN, gpio.DIR_OUT);
+gpio.setup(AM_RESET_PIN, gpio.DIR_OUT, pause);
+
+var sensor = {
+    initialize: function () {
+        return sensorLib.initialize(22, 4);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        setTimeout(function () {
+            sensor.read();
+        }, 2000);
+    }
+};
+
+var amReset = function(){
+  gpio.write(AM_RESET_PIN,0,function(err){
+    if (err) throw err;
+    console.log('RESETTING AM2302');
+  });
+  setTimeout(function(){
+    gpio.write(AM_RESET_PIN,1,function(err){
+      if (err) throw err;
+      console.log('Power On AM2302');
+      setTimeout(startReadings,5000);
+    });
+  },5000);
+}
+var startReadings = function(){
+  setInterval(function(){
+    if (sensor.initialize()) {
+        sensor.read();
+    } else {
+        console.warn('Failed to initialize sensor');
+    }
+  },5000);
+}
