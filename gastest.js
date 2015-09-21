9f856670868647964fe59fca1b1643732bc1a825
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-21 10:49:16 -0400

    update gas test

--- gastest.js
+++ gastest.js
@@ -7,7 +7,7 @@ var offDelay = 90000;
 var count = 0;
 var max   = 3;
 
-//gpio.setup(pin, gpio.DIR_OUT, on);
+gpio.setup(pin, gpio.DIR_OUT, on);
 
 var spi = new SPI.Spi('/dev/spidev0.0', {
     'mode': SPI.MODE['MODE_0'],  // always set mode as the first option
@@ -20,25 +20,27 @@ setInterval(function(){
   getADC(0);
   getADC(1);
 },500);
-// function on() {
-//     // if (count >= max) {
-//     //     gpio.destroy(function() {
-//     //         console.log('Closed pins, now exit');
-//     //     });
-//     //     return;
-//     // }
-//
-//     setTimeout(function() {
-//         gpio.write(pin, 1, off);
-//         count += 1;
-//     }, onDelay);
-// }
-//
-// function off() {
-//     setTimeout(function() {
-//         gpio.write(pin, 0, on);
-//     }, offDelay);
-// }
+function on() {
+    // if (count >= max) {
+    //     gpio.destroy(function() {
+    //         console.log('Closed pins, now exit');
+    //     });
+    //     return;
+    // }
+
+    setTimeout(function() {
+        gpio.write(pin, 1, off);
+        console.log("ON");
+        count += 1;
+    }, onDelay);
+}
+
+function off() {
+    setTimeout(function() {
+        gpio.write(pin, 0, on);
+        console.log("OFF");
+    }, offDelay);
+}
 var getADC = function(channel){
   var spiData =  new Buffer([1,(8+channel) << 4,0]);
   var rxbuf = new Buffer([ 0x00, 0x00, 0x00]);
