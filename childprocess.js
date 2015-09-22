5668a5b714d3a049eab8008fcb638354e877f3bb
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-22 18:40:44 -0400

    add parsing to output from wvdial

--- childprocess.js
+++ childprocess.js
@@ -1,13 +1,25 @@
 var cp = require("child_process");
-var cmd = "/usr/bin/lsusb | /bin/egrep Huawei";
+var ralink ="/usr/bin/lsusb | /bin/egrep Ralink";
+var huawei = "/usr/bin/lsusb | /bin/egrep Huawei";
 var cmd2 = "/usr/bin/wvdial IMEI; exit 0";
-cp.exec(cmd,function(error,stdout,stderr){
+cp.exec(ralink,function(error,stdout,stderr){
+  if (error) throw error;
+  if (stdout) console.log(stdout);
+  if (stderr) throw stderr;
+});
+cp.exec(huawei,function(error,stdout,stderr){
   if (error) throw error;
   if (stdout) console.log(stdout);
   if (stderr) throw stderr;
 });
 cp.exec(cmd2,function(error,stdout,stderr){
   if (error) throw error;
-  if (stdout) console.log(stdout);
+  if (stdout){
+    var search = "^AT\\+CGSN\\n(\\d+)\\n";
+    var search2 = "\^ICCID: \"(\\d+)\"";
+    var wvdialres = stdout.match(search);
+    var iccres = stdout.match(search2);
+    console.log("wvdialres: " + wvdialres.toString() + " iccres: " + iccres.toString());
+  }
   if (stderr) throw stderr;
 });
