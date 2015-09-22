399150832bf8dbf2996505ae6f46a210961df258
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-22 12:47:46 -0400

    fixed typo

--- childprocess.js
+++ childprocess.js
@@ -2,6 +2,6 @@ var cp = require("child_process");
 var cmd = "/usr/bin/lsusb | /bin/egrep Huawei";
 cp.exec(cmd,function(error,stdout,stderr){
   if (error) throw error;
-  if (stout) console.log(stdout);
+  if (stdout) console.log(stdout);
   if (stderr) throw stderr;
 });
