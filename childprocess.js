5f6f907c6c958d03c9a12137329da541415c3072
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-22 12:51:48 -0400

    added second command to childprocess

--- childprocess.js
+++ childprocess.js
@@ -1,6 +1,12 @@
 var cp = require("child_process");
 var cmd = "/usr/bin/lsusb | /bin/egrep Huawei";
+var cmd2 = "/usr/bin/wvdial IMEI; exit 0";
 cp.exec(cmd,function(error,stdout,stderr){
+  if (error) throw error;
+  if (stdout) console.log(stdout);
+  if (stderr) throw stderr;
+});
+cp.exec(cmd2,function(error,stdout,stderr){
   if (error) throw error;
   if (stdout) console.log(stdout);
   if (stderr) throw stderr;
