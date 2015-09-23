c7136b235d5946b9bed6f60e277a6568be357225
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-22 20:51:46 -0400

    updated sensorpost

--- sensorpost.js
+++ sensorpost.js
@@ -19,11 +19,6 @@ var spi = new SPI.Spi('/dev/spidev0.0', {
     s.maxSpeed(1000000);
     s.open();
   });
-  var mq7 = getADC(0);
-  var mq135 = getADC(1);
-  gpio.write(mq7pin, 1, off);
-  var json = {"MQ7":mq7,"MQ135":mq135};
-  sendPost(json,"air");
 function on() {
     setTimeout(function() {
         console.log("-----READING BEFORE ON-----")
@@ -75,3 +70,8 @@ var sendPost = function(jsonData,path){
   }
 });
 }
+var mq7 = getADC(0);
+var mq135 = getADC(1);
+gpio.write(mq7pin, 1, off);
+var json = {"MQ7":mq7,"MQ135":mq135};
+sendPost(json,"air");
