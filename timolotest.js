bd9b2e184cd5cc61866808b6ed09f52052a9fd7f
lpercifield@gmail.com <lpercifield@gmail.com>
2015-09-28 18:44:32 -0400

    added pi-timolo test from node

---
+++ timolotest.js
@@ -0,0 +1,17 @@
+var PythonShell = require('python-shell');
+var timolo = new PythonShell('~/pi-timolo/pi-timolo.py');
+
+// sends a message to the Python script via stdin
+//pyshell.send('hello');
+
+timolo.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+  console.log(message);
+});
+setTimeout(function(){
+  // end the input stream and allow the process to exit
+  timolo.end(function (err) {
+    if (err) throw err;
+    console.log('finished');
+  });
+},60000);
