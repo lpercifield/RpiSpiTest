4310fd23c5b93f384380f0514e79efcad4d6fd9b
lpercifield@gmail.com <lpercifield@gmail.com>
2015-10-14 16:07:38 -0400

    added am2302 module

--- junctionbox.js
+++ junctionbox.js
@@ -1,4 +1,16 @@
 var gpio = require('rpi-gpio');
 var leds = require("./modules/leds.js");
+var tempSensor = require("./modules/am2302.js");
+var mainInterval;
+var mainLoopTime = 60000;
 
 leds.setup(gpio);
+tempSensor.setup(gpio,function(){
+  console.log("tempSensor ready");
+})
+mainInterval = setInterval(mainLoop,mainLoopTime);
+
+function mainLoop(){
+  var tempReading = tempSensor.read();
+  console.log("Temp: "+tempReading.temperature.toFixed(1) + " Humidity: "+tempReading.humidity.toFixed(1));
+}
---
+++ modules/am2302.js
@@ -0,0 +1,77 @@
+var _gpio;
+var sensorLib = require('node-dht-sensor');
+var AM_PIN = 17;
+var AM_RESET_PIN = 15;
+var readingnumber = 0;
+var readingInterval;
+var readingTime = 60000;
+
+//console.log("before gpio setup");
+//gpio.setup(AM_PIN, gpio.DIR_OUT);
+exports.setup = function(gpio,callback){
+  _gpio = gpio;
+  _gpio.setup(AM_RESET_PIN, _gpio.DIR_OUT, function(err){
+    if (err) throw err;
+    //console.log("Calling amreset");
+    amReset(callback);
+  });
+}
+//console.log("after gpio setup");
+
+var sensor = {
+    sensors: [ {
+        name: "Indoor",
+        type: 22,
+        pin: AM_PIN
+    }],
+    read: function() {
+        for (var a in this.sensors) {
+            var b = sensorLib.readSpec(this.sensors[a].type, this.sensors[a].pin);
+            readingnumber++;
+            return b;
+            // console.log("Reading Number: "+ readingnumber+" " +this.sensors[a].name + ": " +
+            //   b.temperature.toFixed(1) + "C, " +
+            //   b.humidity.toFixed(1) + "%" + " Errors: " + b.errors + " isValid: " + b.isValid);
+        }
+    }
+};
+
+exports.reset = function(callback){
+  amReset(callback);
+}
+exports.read = function(){
+  try {
+    return sensor.read();
+  } catch (e) {
+    console.error(e)
+    //clearInterval(readingInterval);
+    amReset();
+  }
+}
+
+
+var amReset = function(callback){
+  console.log("Starting AMRESET")
+  _gpio.write(AM_RESET_PIN,0,function(err){
+    if (err) throw err;
+    console.log('RESETTING AM2302');
+  });
+  setTimeout(function(){
+    _gpio.write(AM_RESET_PIN,1,function(err){
+      if (err) throw err;
+      console.log('Power On AM2302');
+      setTimeout(callback,1000);
+    });
+  },5000);
+}
+var startReadings = function(){
+  readingInterval = setInterval(function(){
+    try {
+      sensor.read();
+    } catch (e) {
+      console.log(e)
+      clearInterval(readingInterval);
+      amReset();
+    }
+  },5000);
+}
