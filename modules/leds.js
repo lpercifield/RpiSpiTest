67cbd4ed200b3894d3a2ca56c2f0efbe616d3d4d
lpercifield@gmail.com <lpercifield@gmail.com>
2015-10-14 15:44:50 -0400

    started junctionbox firmware and modules

--- modules/leds.js
+++ modules/leds.js
@@ -2,14 +2,14 @@ var _gpio,
   yellowLed = 40,
   redLed = 38,
   heartbeatDelayTime = 5000,
-  heartbeatOnTime = 250;
+  heartbeatOnTime = 1000;
 exports.setup = function(gpio){
   _gpio = gpio;
   _gpio.setup(yellowLed, _gpio.DIR_OUT,heartbeat);
   _gpio.setup(redLed, _gpio.DIR_OUT);
 }
 function heartbeat(){
-  _gpio.write(yellowLed, false);
+  //_gpio.write(yellowLed, false);
   setTimeout(function() {
     _gpio.write(yellowLed, false, function(err) {
       if (err) throw err;
