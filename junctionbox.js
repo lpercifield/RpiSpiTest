2b25ff2bc86deb2c622ff057cac69314004282b5
lpercifield@gmail.com <lpercifield@gmail.com>
2015-10-16 13:35:54 -0400

    added usb devices module and timolo (currently disabled)

--- junctionbox.js
+++ junctionbox.js
@@ -1,25 +1,53 @@
 var gpio = require('rpi-gpio');
 var SPI = require('spi');
 var leds = require("./modules/leds.js");
+var PythonShell = require('python-shell');
 var tempSensor = require("./modules/am2302.js");
 var gasSensors = require("./modules/gasSensors.js");
+var usbDevices = require("./modules/usbDevices.js");
 var mainInterval;
 var mainLoopTime = 60000;
 
-leds.setup(gpio);
+usbDevices.deviceIds(function(err,results){
+  console.log(JSON.stringify(results));
+});
 
+// NOTE: setup LEDS
+leds.setup(gpio);
+// NOTE: setup tempSensor
 tempSensor.setup(gpio,function(){
   console.log("tempSensor ready");
-})
+});
+// // NOTE: options for setting up pi-timolo
+// var options = {
+//   mode: 'json',
+//   scriptPath: '/home/pi/pi-timolo'
+// };
+// // NOTE: create timolo instance
+// var timolo = new PythonShell('pi-timolo.py',options);
+//
+// // NOTE: register timolo message event
+// timolo.on('message', function (message) {
+//   // received a message sent from the Python script (a simple "print" statement)
+//   console.log("Timolo " + message.hasOwnProperty('timolo'));
+//   console.log("Motion " + message.hasOwnProperty('motion'));
+//   console.log("Timelapse " + message.hasOwnProperty('timelapse'));
+//   console.log(message);
+// });
 
+// NOTE: register events from gas sensors
 gasSensors.events.on('ready', function(sensorData) {
   console.log('gasSensors data is ready');
   console.log(JSON.stringify(sensorData));
 });
+
+// NOTE: setup gas sensor
 gasSensors.setup(gpio,SPI);
 
+// NOTE: setup main loop interval
 mainInterval = setInterval(mainLoop,mainLoopTime);
 
+// NOTE: function that runs when the main interval expires
 function mainLoop(){
   var tempReading = tempSensor.read();
   console.log("Temp: "+tempReading.temperature.toFixed(1) + " Humidity: "+tempReading.humidity.toFixed(1));
---
+++ modules/usbDevices.js
@@ -0,0 +1,66 @@
+var async = require("async");
+var cp = require("child_process");
+var ralinkcmd ="/usr/bin/lsusb | /bin/egrep Ralink";
+var huaweicmd = "/usr/bin/lsusb | /bin/egrep Huawei";
+var cellcmd = "/usr/bin/wvdial IMEI; exit 0";
+
+exports.deviceIds = function(error,callbackMain){
+  async.parallel({
+    ralink: function(callback){
+      cp.exec(ralinkcmd,function(error,stdout,stderr){
+        if (error) throw error;
+        if (stdout){
+          var str = stdout.toString().trim();
+          console.log(str);
+          callback(null,str);
+        }
+        if (stderr) throw stderr;
+      });
+    },
+    huawei: function(callback){
+      cp.exec(huaweicmd,function(error,stdout,stderr){
+        if (error) throw error;
+        if (stdout){
+          var str = stdout.toString().trim();
+          console.log(str);
+          callback(null,str);
+        }
+        if (stderr) throw stderr;
+      });
+    },
+    cell: function(callback){
+      cp.exec(cellcmd,function(error,stdout,stderr){
+        if (error) throw error;
+        if (stdout){
+          console.log(stderr);
+          var imeiRX= /\nAT\+CGSN\n(\d+)\n/
+          var iccidRX = /\^ICCID: (\d+)\n/;
+          var imei = stdout.match(imeiRX);
+          var iccid = stdout.match(iccidRX);
+          var obj = {};
+          obj["imei"] = imei;
+          obj["iccid"] = iccid;
+          callback(null,obj);
+          console.log("imei: " + imei[1] + " iccid: " + iccid[1]);
+        }
+        if (stderr){
+          console.log(stderr);
+          var imeiRX= /\nAT\+CGSN\n(\d+)\n/
+          var iccidRX = /\^ICCID: (\d+)\n/;
+          var imei = stderr.match(imeiRX);
+          var iccid = stderr.match(iccidRX);
+          var obj = {};
+          obj["imei"] = imei;
+          obj["iccid"] = iccid;
+          callback(null,obj);
+          console.log("imei: " + imei[1] + " iccid: " + iccid[1]);
+        }// throw stderr;
+      });
+    }
+},
+function(err, results) {
+    // results is now equals to: {one: 1, two: 2}
+    callbackMain(err,results);
+});
+
+}
