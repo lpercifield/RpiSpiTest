b6491d3c97c10f8b56f17e6fc605e97a8ee9044b
lpercifield@gmail.com <lpercifield@gmail.com>
2015-10-16 14:31:42 -0400

    fixed callback from usb devices

--- modules/usbDevices.js
+++ modules/usbDevices.js
@@ -11,7 +11,7 @@ exports.deviceIds = function(callbackMain){
         if (error) throw error;
         if (stdout){
           var str = stdout.toString().trim();
-          console.log(str);
+          //console.log(str);
           callback(null,str);
         }
         if (stderr) throw stderr;
@@ -22,7 +22,7 @@ exports.deviceIds = function(callbackMain){
         if (error) throw error;
         if (stdout){
           var str = stdout.toString().trim();
-          console.log(str);
+          //console.log(str);
           callback(null,str);
         }
         if (stderr) throw stderr;
@@ -32,7 +32,7 @@ exports.deviceIds = function(callbackMain){
       cp.exec(cellcmd,function(error,stdout,stderr){
         if (error) throw error;
         if (stdout){
-          console.log(stderr);
+          //console.log(stderr);
           var imeiRX= /\nAT\+CGSN\n(\d+)\n/
           var iccidRX = /\^ICCID: (\d+)\n/;
           var imei = stdout.match(imeiRX);
@@ -44,7 +44,7 @@ exports.deviceIds = function(callbackMain){
           console.log("imei: " + imei[1] + " iccid: " + iccid[1]);
         }
         if (stderr){
-          console.log(stderr);
+          //console.log(stderr);
           var imeiRX= /\nAT\+CGSN\n(\d+)\n/
           var iccidRX = /\^ICCID: (\d+)\n/;
           var imei = stderr.match(imeiRX);
