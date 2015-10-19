c0a25bd6e9c69e8495b1f3e80fe60e9fcaf88895
lpercifield@gmail.com <lpercifield@gmail.com>
2015-10-19 17:15:50 -0400

    added am2302 value check in setup

--- modules/am2302.js
+++ modules/am2302.js
@@ -16,7 +16,8 @@ exports.events = events;
 //gpio.setup(AM_PIN, gpio.DIR_OUT);
 exports.setup = function(gpio,dataPin,resetPin,callback){
   _gpio = gpio;
-  AM_PIN = parseInt(dataPin);
+  //AM_PIN = parseInt(dataPin);
+  AM_PIN = 17;
   //AM_RESET_PIN = parseInt(resetPin);
   AM_RESET_PIN = 15;
   _gpio.setup(AM_RESET_PIN, _gpio.DIR_OUT, function(err){
